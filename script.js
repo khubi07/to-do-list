073bb757d79f570673f94c5d93bf4969e2fc8e49
khubi sahu <vkhubisahu14@gmail.com>
2025-02-24 16:50:02 +0530

    js added

--- script.js
+++ script.js
@@ -1,9 +1,54 @@
 // lets add variable
-const inputBox = document.getElementById("searchBox");
-const listContainer = document.getElementById("list-container");
+const inputBox = document.getElementById("inputBox");
+const listContainer = document.getElementById("listContainer");
 
-function AddTask(){
+function AddTask(event){
+    //prevent the default form submission behaviour
+    if(event){
+        event.preventDefault();
+    }
+
     if(inputBox.value === ''){
         alert("you must write something!");
     }
+    else{
+        //new list to set its inner HTML the value of inputBox
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+
+        //append new list item to list container
+        listContainer.appendChild(li);
+
+        //create span ele, set its innerHTML to "x" and append to list itme
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+
+    }
+    //clear value of input box
+    inputBox.value = "";
+    saveData();
 }
+//make sure id in html and js matches to work properly
+
+//js for click function
+listContainer.addEventListener("click", function(e){
+    if(e.target.tagName ==="LI"){
+        e.target.classList.toggle("checked");
+        saveData();
+    }
+    else if(e.target.tagName ==="SPAN"){
+        e.target.parentElement.remove();
+        saveData();
+    }
+},false);
+
+//function to save data
+function saveData(){
+    localStorage.setItem("data",listContainer.innerHTML);
+}
+//to display data again
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
